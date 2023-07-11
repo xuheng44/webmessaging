328226d71f7e795c415d0dec2591fb567cb0c484
Sky <89450349+xuheng44@users.noreply.github.com>
2023-07-11 10:31:02 +0800

    Update inviteform.js

--- inviteform.js
+++ inviteform.js
@@ -63,6 +63,7 @@ let launcher = document.createElement('button')
 launcher.onclick = function () {
   openLauncher()
 }
+/*  hidden by sky
 launcher.style = `cursor: pointer;
       box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 5px -2px, rgba(0, 0, 0, 0.14) 0px 1px 4px 2px, rgba(0, 0, 0, 0.12) 0px 1px 4px 1px;
       position: fixed !important;
@@ -76,7 +77,7 @@ launcher.style = `cursor: pointer;
       border: 0px;`
 launcher.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 12H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z"/></svg>`
 document.body.appendChild(launcher)
-
+hidden by sky */
 //Create Input Form
 let input = document.createElement('div')
 let header = document.createElement('div')
