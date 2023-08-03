77b77560fa31331bd6ff84ef489ca0ed18133e0d
Sky <89450349+xuheng44@users.noreply.github.com>
2023-08-03 09:24:05 +0800

    commit

--- inviteform.js
+++ inviteform.js
@@ -1,7 +1,7 @@
 'use strict'
 
 //Variables to change in your deployment
-const deploymentId = '171a5200-a8e9-4fe6-8289-966ae6be547d' //Your WebMessenger DeploymentId
+const deploymentId = 'b9c07add-2ac6-45f5-8be8-bf608e215f3f' //Your WebMessenger DeploymentId 171a5200-a8e9-4fe6-8289-966ae6be547d
 const hexColor = '#7985E3' //Color theme
 
 function toggleMessenger() {
